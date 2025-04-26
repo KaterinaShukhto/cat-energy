a4fb73c7f7a4f06967e209f28d9f3352059989c6
Kirill Samoilov <samsak.developer@internet.ru>
2025-04-26 13:25:07 +0300

    Processing form page (added full elements -> next adaptive)

--- scripts/openMobileMenu.js
+++ scripts/openMobileMenu.js
@@ -4,4 +4,9 @@ const mobileMenu = document.querySelector(".mobile__box_navigate");
 burgerBtn.addEventListener("click", () => {
   burgerBtn.classList.toggle("active");
   mobileMenu.classList.toggle("hidden");
+  if (burgerBtn.classList.contains("active")) {
+    document.body.style.overflow = "hidden"; // Блокировка прокрутки
+  } else {
+    document.body.style.overflow = ""; // Возврат прокрутки
+  }
 });
